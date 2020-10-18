6d1993297fba34094c5925959215d5b4d64d55e6
sousajo <josesousa@rentalcars.com>
2020-10-18 17:25:22 +0100

    Styled the navbar with css and added some js to change classes for responsiveness.Closes #4

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,14 +5,20 @@ import '../icons/fontawesome';
 
 
 class Navbar extends Component {
+    state = {clicked: false}
+    handleClicked = () => {
+        this.setState({clicked: !this.state.clicked})
+    }
+
+
     render() {
         return (
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">React<i className="fab fa-react"></i></h1>
-                <div className="menu-icon">
-
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
                 </div>
-                <ul>
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
                     {MenuItems.map((item, index) => {
                         return (
                             <li key={index}><a className={item.cname} href={item.url}>{item.title}</a></li>
