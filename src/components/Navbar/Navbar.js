3c1fa5c7af9651ba7aaff3215abe8b666705f4a0
sousajo <josesousa@rentalcars.com>
2020-10-18 16:53:28 +0100

    Added fontawesome to project and icon to logo.Closes #3

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,17 @@
 import React, {Component} from "react";
 import {MenuItems} from "./MenuItems";
+import './Navbar.css'
+import '../icons/fontawesome';
+
 
 class Navbar extends Component {
     render() {
         return (
             <nav className="NavbarItems">
-                <h1 className="navbar-logo"> React</h1>
-                <div className="menu-icon"></div>
+                <h1 className="navbar-logo">React<i className="fab fa-react"></i></h1>
+                <div className="menu-icon">
+
+                </div>
                 <ul>
                     {MenuItems.map((item, index) => {
                         return (
